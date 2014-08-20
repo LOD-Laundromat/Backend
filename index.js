693ed2117c09a9ebb63ef9cac83a7f6c9e61d1a1
Laurens Rietveld <laurens.rietveld@gmail.com>
2014-08-20 19:12:59 +0200

    improved catchall to avoid not sending response

--- index.js
+++ index.js
@@ -17,9 +17,11 @@ if (!fs.existsSync(config.loggingDir)) throw new Error("Logging dir does not exi
 http.createServer(function (req, res) { 
 	if (req.url.indexOf("/data/") == 0) {
 		serveDataFile(req,res);
+	} else if (req.url.indexOf("/addseed/" == 0)) {
+	} else {
+		res.writeHead(400);//catchall
+		res.end();
 	}
-//	res.writeHead(400);//catchall
-//	res.end();
 }).listen(config.port);
 
 util.puts('> LOD Laundromat Backend running on ' + config.port);
