4644676c7c6075ee4f237db8a2f533cbd2ab7819
Laurens Rietveld <laurens.rietveld@gmail.com>
2014-08-21 10:30:16 +0200

    connected seed list API to function storing the seed item

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ var util = require('util'),
 	fs = require('fs'),
 	url = require('url'),
 	path = require('path'),
+	seedlistUpdater = require('./sendSeedItem.js'),
 	config = require('./config.json');
 
-if (!config.dataDir) throw new Error('No data directory defined');
-if (!fs.existsSync(config.dataDir)) throw new Error('Data directory not found: ' + config.dataDir);
-if (!config.filehostingPort) throw new Error('No port defined to run  file hosting server on');
-if (!config.llVersion) throw new Error('No version defined to serve files for');
-if (!config.endpoint) throw new Error('No endpoint defined to query');
+if (!config.fileHosting.dataDir) throw new Error('No data directory defined');
+if (!fs.existsSync(config.fileHosting.dataDir)) throw new Error('Data directory not found: ' + config.dataDir);
+if (!config.fileHosting.port) throw new Error('No port defined to run  file hosting server on');
+if (!config.fileHosting.llVersion) throw new Error('No version defined to serve files for');
+
 if (!config.loggingDir) throw new Error("No logging dir specified");
 if (!fs.existsSync(config.loggingDir)) throw new Error("Logging dir does not exist");
+
+
 
 
 /**
@@ -38,7 +41,7 @@ http.createServer(function (req, res) {
 		res.writeHead(406, 'No dataset defined in download request');
 		res.end();
 	} else {
-		var datasetDir = config.dataDir + '/' + config.llVersion + '/' + hash;
+		var datasetDir = config.fileHosting.dataDir + '/' + config.fileHosting.llVersion + '/' + hash;
 		fs.exists(datasetDir, function(datasetDirExists) {
 			if (!datasetDirExists) {
 				console.log(datasetDir);
@@ -69,10 +72,47 @@ http.createServer(function (req, res) {
 			
 		});
 	}
-}).listen(config.filehostingPort);
-util.puts('> File hosting backend running on ' + config.filehostingPort);
+}).listen(config.fileHosting.port);
+util.puts('> File hosting backend running on ' + config.fileHosting.port);
 
 
 /**
  * Run seed list updater server
 */
+
+
+if (!config.seedlistUpdater.graphApi) throw new Error('No graph API URL defined to send new seed list items to');
+if (!config.seedlistUpdater.namedGraph) throw new Error('No named graph defined to store new seed list items in');
+if (!config.seedlistUpdater.port) throw new Error('No port defined to run seed list API on');
+http.createServer(function (req, res) { 
+//	seedlistUpdater
+
+	var seed = (url.parse(req.url, true).query).url;
+	if (!seed || seed.trim().length == 0) {
+		res.writeHead(400, 'No seed item given as argument');
+		res.end();
+	} else {
+		var parsedSeed = url.parse(seed.trim());
+		//validate seed
+		console.log(parsedSeed);
+		if (parsedSeed.protocol && parsedSeed.protocol.indexOf('http') == 0) {
+			//only pass along the parsed href! (this avoids injection into our turtle insert)
+			seedlistUpdater(parsedSeed.href, function(success, body) {
+				if (success) {
+					res.writeHead(200, 'Successfully added ' + parsedSeed.href + ' to the seed list');
+					
+				} else {
+					res.writeHead(500, 'Failed to add ' + parsedSeed.href + " to the seed list");
+					res.write(body);
+					res.end();
+				}
+				
+			});
+		} else {
+			res.writeHead(400, 'The seed item is not a URI: ' + seed);
+			res.end();
+		}
+	}
+	
+}).listen(config.seedlistUpdater.port);
+util.puts('> Seed list backend running on ' + config.seedlistUpdater.port);
--- sendSeedItem.js
+++ sendSeedItem.js
@@ -26,4 +26,3 @@ module.exports = function(seedUrl, callback) {
     });
 };
 
-module.exports();
