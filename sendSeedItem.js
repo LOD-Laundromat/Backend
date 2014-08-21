871a9deb98e69f984605576bb4e26086f89402ec
Wouter Beek <w.g.j.beek@vu.nl>
2014-08-21 10:00:47 +0200

    finished seed list implementation

--- sendSeedItem.js
+++ sendSeedItem.js
@@ -1,20 +1,29 @@
-var config = require('./config.json'),
+var 	config = require('./config.json'),
+	http = require('http'),
+	request = require('request'),
 	queryString = require('querystring');
-module.exports = function(seedUrl) {
-	var post_options = {
-	      host: config.endpoint,
-	      path: "/?"+ queryString.stringify({"using-named-graph-uri": "http://testlodlaundromat.org#" + llVersion}),
-	      port: '80',
-	      method: 'POST',
-	      headers: {
-	          'Content-Type': 'text/turtle',
-	      }
-	  };
-	var req = http.request(post_options, function(response){
-		
-	});
-	req.write("<http://test111> <http://test222> <http://test333> .");
-	req.end();
+
+
+
+module.exports = function(seedUrl, callback) {
+    var options = {
+	url: config.endpoint + '?' + queryString.stringify({"graph-uri": "http://lodlaundromat.org#seedlist"}),
+	headers: {
+            'Content-Type': 'text/turtle'
+	},
+	method: 'POST',
+	body: "<http://test111> <http://test222> <http://test333> ."
+    };
+    
+    var req = request(options, function(err, response, body){
+	if (err) {
+	    callback(false, body);
+	} else if (response.statusCode != 200) {
+	    callback(false, body);
+	} else {
+	    callback(true);
+	}
+    });
 };
 
-//module.exports();
+module.exports();
