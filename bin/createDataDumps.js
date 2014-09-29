cd207facfc8f4495a99c595af2f32099731e5a6b
Laurens Rietveld <laurens.rietveld@gmail.com>
2014-09-30 00:43:45 +0200

    initial implementation of data dump generator

--- bin/createDataDumps.js
+++ bin/createDataDumps.js
@@ -2,9 +2,42 @@
 
 var fs = require('fs'),
 	config = require('../config.json'),
+	shell = require('shelljs'),
 	queryString = require('querystring');
 
-//dump_ntriples('http://lodlaundromat.org#metrics-11', '/scratch/lodlaundromat/dumps//metrics/', 1000000000)
-console.log(config);
 
+
+if (!fs.existsSync(config.datadumps.dumpLocation)) throw new Error("Dump location does not exist: " + config.datadumps.dumpLocation);
+
+
+console.log("cleaning old dumps");
+shell.rm('-r', config.datadumps.dumpLocation + '/*');
+
+
+var graphs = config.datadumps.graphs
+
+
+
+var createDataDump = function(graphsToDo) {
+	var graphUri = null;
+	for (graphUri in graphsToDo) break;
+	if (graphUri == null) {
+		console.log("> no graphs left to create data dump for");
+	} else {
+		var targetFileName = graphsToDo[graphUri];
+		console.log("> creating dump for graph " + graphUri);
+		var targetPath = config.datadumps.dumpLocation + "/" + targetFileName;
+		var singleDumpFile = config.datadumps.dumpLocation + "/" + targetFileName + ".gz";
+		shell.mkdir(targetPath);
+		
+		exec('isql exec="dump_ntriples(\'' + graphUri + '\', \'' + targetPath + '\')"', function(exitCode, output) {
+			console.log(">> finished exporting dumps. Creating single file");
+			exec('cat ' + targetPath + '/* >> ' + singleDumpFile, function(exitCode, output) {
+				console.log(">> finished creating single file");
+				delete graphsToDo[graphUri];
+				createDataDump(graphsToDo);
+			});
+		});
+	}
+};
 
