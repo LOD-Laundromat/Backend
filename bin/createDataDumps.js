b18c7c08ff72cbb5bc15da94d99cd8823beea85e
Laurens Rietveld <laurens.rietveld@gmail.com>
2014-09-30 00:13:51 +0200

    fixed parse issues

--- bin/createDataDumps.js
+++ bin/createDataDumps.js
@@ -4,6 +4,7 @@ var fs = require('fs'),
 	config = require('../config.json'),
 	queryString = require('querystring');
 
+//dump_ntriples('http://lodlaundromat.org#metrics-11', '/scratch/lodlaundromat/dumps//metrics/', 1000000000)
 console.log(config);
 
 
