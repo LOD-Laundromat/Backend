2192d2ad782ded399d5dd1ed4fe5a77d17a53857
Wouter Beek <w.g.j.beek@vu.nl>
2015-02-18 11:41:00 +0100

    added some utility scripts to repo

---
+++ bin/wm-callback.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+hdtQueue="/scratch/lodlaundromat/tmp/hdtQueue.txt"
+#touch /home/lodlaundromat/wm-callback.touch
+[ -z "$1" ] && echo "No dataset provided as argument" && exit 1;
+
+date >> /home/lodlaundromat/wm-callback.touch
+echo $@ >> /home/lodlaundromat/wm-callback.touch;
+
+
+
+echo "Generating HDT file ($1)"
+#makeHdt $1
+
+#queue hdt file for ldf update
+echo $1 >> $hdtQueue;
+
+#analyze directory
+echo "Creating C-LOD file ($1)"
+streamDataset $1
+
+
